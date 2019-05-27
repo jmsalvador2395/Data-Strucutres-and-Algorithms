af531280fb0f59e211fa4078f8218e09f89d3301
jmsalvador2395 <jmsalvador2395@gmail.com>
2019-05-27 10:55:16 -0700

    finished rotation functions

--- redblacktree/main.py
+++ redblacktree/main.py
@@ -3,5 +3,5 @@ import redblacktree as rbt
 def main():
 	a=rbt.RedBlackTree()
 	print a.root.color
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
--- redblacktree/redblacktree.py
+++ redblacktree/redblacktree.py
@@ -1,5 +1,3 @@
-
-
 class RedBlackTree:
 	class _node:
 		def __init__(self):
@@ -24,25 +22,51 @@ class RedBlackTree:
 	#in the middle of implementing
 	def _insert(self, key, node):
 		return
+
 	#in the middle of implementing
-	def insert(key):
+	def insert(self, key):
 		if(this.root.data is None):
 			this.root.data = key
 		if(key>this.root.key):
 			_insert(self, key, root.right)
 		elif(key<this.root.key):
 			_insert(self, key, root.left)
+	
+	def _black_height(self):
+		return
 			
 	#need to implement
 	def delete(self, key):
 		return
 		
-	def _left_rotate(self, node):
-		temp=node.right
-		node.right=temp.left
-		if(temp.left is not None):
-			temp.left.p=node
-		temp.p=node.p
-		if(node.p is None):
-			self.root=temp
-		#elif(node == 						#<<<<<<<<<<<<<<<<<<<<<<<<
+	def _left_rotate(self, x):
+		y=x.right					#make ptr to right child
+		x.right=y.left				#change right child of x
+		if(y.left is not None):		#make x the parent of y.left if not null
+			y.left.p=x
+		y.p=x.p						#set y's parent to x's parent
+		if(x.p is None):			#check if x's parent is root
+			self.root=y
+		elif(x==x.p.left):			#check if x was a left node
+			x.p.left=y
+		else:						#x is resolved to be a right child
+			x.p.right=y
+		y.left=x
+		x.p=y
+	
+	def _right_rotate(self, y):
+		x=y.left
+		y.left=x.right
+		if(x.right is not None):
+			x.right.p=y.left
+		x.p=y.p
+		if(y.p is None):
+			self.root=x
+		else if(y==y.p.left):
+			y.p.left=x
+		else:
+			y.p.right=x
+		x.right=y
+		y.p=x
+		return
+
