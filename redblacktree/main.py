b0ed1084e1d10ac1f92c1c5e5e061a25b1c16749
John <jmsalvador2395@gmail.com>
2019-06-02 21:17:56 -0700

    idk what i changed but i'm pretty sure it still works

--- redblacktree/main.py
+++ redblacktree/main.py
@@ -3,10 +3,13 @@ import random
 
 def main():
 	rbt=redblacktree.RedBlackTree()
+	'''
 	inp=random.sample(range(1, 100), 10)
+	'''
 	'''
 	inp=[1,0,2]
 	'''
+	inp=[41,38,31,12,19,8]
 	for i in inp:
 		print 'insert ' + str(i)
 		rbt.rb_insert(i)
